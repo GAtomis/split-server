82eea5a5db552b8ac80514ca2dfeb177e79c78bc
Gavin <zhounan@agora.io>
2022-09-29 10:30:09 +0800

    update@MD

--- model/business/request/model_charts.go
+++ model/business/request/model_charts.go
@@ -2,11 +2,11 @@
  * @Description: 请输入....
  * @Author: Gavin
  * @Date: 2022-09-26 17:12:40
- * @LastEditTime: 2022-09-26 18:50:38
+ * @LastEditTime: 2022-09-29 10:29:47
  * @LastEditors: Gavin
  */
 package request
 
-type Login struct {
+type QuarterOrMonth struct {
 	Fag uint `json:"fag"` //1为月 2为季度
 }
